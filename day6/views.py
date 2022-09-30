ebaced0ce6580ec71223dbb163e5e59765f412a4
an dong woo <akskwha@naver.com>
2022-09-30 13:04:18 +0900

    중간push

--- day6/views.py
+++ day6/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, "day6/index.html")
+
+
+def create(request):
+    return render(request, "day6/create.html")
