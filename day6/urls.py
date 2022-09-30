30c978a6518045f264fbcabf144b5c2119bc3a9a
an dong woo <akskwha@naver.com>
2022-09-30 16:43:10 +0900

    완성

--- day6/urls.py
+++ day6/urls.py
@@ -5,6 +5,11 @@ from . import views
 app_name = "day6"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("index/", views.index, name="index"),
     path("create/", views.create, name="create"),
+    path("created/", views.created, name="created"),
+    path("detail/<int:pk>/", views.detail, name="detail"),
+    path("delete/<int:pk>/", views.delete, name="delete"),
+    path("edit/<int:pk>/", views.edit, name="edit"),
+    path("editted/<int:pk>/", views.editted, name="editted"),
 ]
--- day6/views.py
+++ day6/views.py
@@ -1,11 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Review
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "day6/index.html")
+
+    context = {
+        "reviews": Review.objects.all().order_by("id"),
+    }
+
+    return render(request, "day6/index.html", context)
 
 
 def create(request):
     return render(request, "day6/create.html")
+
+
+def created(request):
+
+    title = request.GET.get("title")
+    content = request.GET.get("content")
+
+    Review.objects.create(title=title, content=content)
+
+    return redirect("day6:index")
+
+
+def detail(request, pk):
+
+    context = {
+        "review": Review.objects.get(pk=pk),
+    }
+    return render(request, "day6/detail.html", context)
+
+
+def delete(request, pk):
+    Review.objects.get(pk=pk).delete()
+    return redirect("day6:index")
+
+
+def edit(request, pk):
+    context = {"edit": Review.objects.get(pk=pk)}
+    return render(request, "day6/edit.html", context)
+
+
+def editted(request, pk):
+
+    new_title = request.GET.get("edit-title")
+    new_content = request.GET.get("edit-content")
+
+    review = Review.objects.get(pk=pk)
+
+    review.title = new_title
+    review.content = new_content
+    review.save()
+
+    return redirect("day6:index")
